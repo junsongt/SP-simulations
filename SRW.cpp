25c2c0beea7753c7c93edcd92642a6570aac2d9f
junsongt <junsongt@students.cs.ubc.ca>
2023-07-07 12:04:36 -0700

    update SRW

--- SRW.cpp
+++ SRW.cpp
@@ -24,18 +24,20 @@ namespace plt = matplotlibcpp;
 int main() {
     // wall time
     auto start = chrono::high_resolution_clock::now();
-
-    double prob = 0.5;
-
-    std::random_device gen;
     // std::minstd_rand gen(12345);
     // std::default_random_engine gen;
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    // number of steps
+    int n = 100000;
 
+    // 1_D case:
+    double prob = 0.5;
     std::bernoulli_distribution ber(prob);
 
-    int n = 100000;
     int X_i = 0;
-    vector<int> X;
+    vector<int> X = {X_i};
+    vector<int> time = {0};
 
     for (int i = 1; i <= n; i++) {
         bool b = ber(gen);
@@ -47,9 +49,35 @@ int main() {
         }
         X_i = X_i + Z_i;
         X.push_back(X_i);
+        time.push_back(i);
     }
 
     plt::plot(X);
+    plt::show();
+
+    // 2-D case
+    int Xi = 0;
+    int Yi = 0;
+    vector<int> Xs = {Xi};
+    vector<int> Ys = {Yi};
+
+    std::uniform_int_distribution<> unif(0, 3);
+
+    // if we use cts unif dist, then dir = 0 when 0 <= dir < 0.25; dir = 1 when 0.25 <= dir < 0.5...
+
+    for (int i = 1; i <= n; i++) {
+        int dir = unif(gen);
+        int dXi = (int)cos((M_PI / 2) * dir);
+        int dYi = (int)sin((M_PI / 2) * dir);
+
+        Xi = Xi + dXi;
+        Yi = Yi + dYi;
+        Xs.push_back(Xi);
+        Ys.push_back(Yi);
+    }
+
+    plt::plot(Xs, Ys);
+    plt::title("2D SRW without time progress");
     plt::show();
 
     auto end = chrono::high_resolution_clock::now();
